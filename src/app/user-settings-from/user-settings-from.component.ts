a56d403b5e1738dc0ab67dd441de63939bc62999
Anwar Ul-Haq <aulhaq6@gmail.com>
2019-07-23 08:03:09 +0100

    added validation

--- src/app/user-settings-from/user-settings-from.component.ts
+++ src/app/user-settings-from/user-settings-from.component.ts
@@ -9,11 +9,11 @@ import { UserSettings } from '../data/user-settings';
 export class UserSettingsFromComponent implements OnInit {
 
   originaluserSettings: UserSettings = {
-    name: 'Anwar Ul-Haq',
-    emailOffers: true,
-    interfaceStyle : 'Dark',
-    subscriptionType: 'Annual',
-    notes: 'here are some notes..'
+    name: null,
+    emailOffers: null,
+    interfaceStyle : null,
+    subscriptionType: null,
+    notes: null
   };
 
   userSettings: UserSettings = {...this.originaluserSettings};
